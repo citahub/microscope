7c75de12747813bdab189217ad52582e74007276
Yao <yao@cryptape.com>
2018-10-12 14:19:56 +0800

    fix: add loading class

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 
+const styles = require('./icons.scss')
+
 const IconBase = ({ name, className = '' }) => (
   <svg className={`icon ${className}`} aria-hidden="true">
     <use xlinkHref={`#icon-${name}`} />
   </svg>
 )
 
-const Loading = () => <IconBase name="loading" className='loading' />
+const Loading = () => <IconBase name="loading" className={styles.loading} />
 
 export { Loading }
 
